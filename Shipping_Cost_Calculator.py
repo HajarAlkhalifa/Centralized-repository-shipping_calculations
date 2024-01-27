cabdf5a3141a7fa4cf18df689df971da29d5d63f
HajarAlkhalifa <hajarhashimx2@gmail.com>
2024-01-27 13:48:40 -0500

    added new lines

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -12,3 +12,4 @@ print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by HajarAlkhalifa
  #adding more lines since it is saying everything is up to date
+ # Here is another update by Hajaralkhalifa
